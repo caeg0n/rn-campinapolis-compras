9037111883d1f536e5dea0d1f5c9ad791a2a6608
caeg0n <caegon2005@hotmail.com>
2023-08-28 21:37:10 -0300

    update

--- src/components/elements/TextField/TextField.js
+++ src/components/elements/TextField/TextField.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { TextInput, I18nManager } from 'react-native';
 import { Box } from '../Box';
--- src/screens/AddAddress/AddAddress.js
+++ src/screens/AddAddress/AddAddress.js
@@ -1,71 +1,25 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
-// import TextInputMask from 'react-native-text-input-mask';
-// import { MaskedTextInput } from 'react-native-mask-text';
-// import Constants from 'expo-constants';
-// import { mapping, light, dark } from '@eva-design/eva';
-// import { ApplicationProvider } from 'react-native-ui-kitten';
+import { Text } from 'react-native';
 
-// function formatPhoneNumber(text) {
-//   const numericText = text.replace(/\D/g, '');
-//   const formattedPhoneNumber = numericText.replace(
-//     /(\d{3})(\d{3})(\d{4})/,
-//     '($1) $2-$3',
-//   );
-//   console.log(formattedPhoneNumber);
-//   return formattedPhoneNumber;
-// }
-
-import {
-  TextField,
-  Button,
-  List,
-  Divider,
-  ListRowItem,
-  Box,
-} from '@src/components';
+import { TextField, Button, Divider, Box } from '@src/components';
 //import { savedAddresses } from '@src/data/mock-address';
 
 export const AddAddress = () => {
-  // const [theme, setTheme] = React.useState('light');
   const [title, setTitle] = React.useState('');
   const [phone, setPhone] = React.useState('');
-  const [address, setAddres] = React.useState('');
-  const [status, setStatus] = React.useState(0);
+  const [address, setAddress] = React.useState('');
+  const [status, setStatus] = React.useState(false);
 
   const saveClick = () => {
-    setStatus(0);
-    if (title === '') {
-      setStatus(1);
+    setStatus(true);
+    if (title !== '') {
+      if (phone !== '') {
+        if (address !== '') {
+          console.log('bingo');
+        }
+      }
     }
     return null;
-  };
-  // const toggleTheme = () => {
-  //   const nextTheme = theme === 'light' ? 'dark' : 'light';
-  //   setTheme(nextTheme);
-  // };
-
-  const prepareListData = (addresses) => {
-    return addresses.map((item) => {
-      const { id, description, name } = item;
-      return {
-        id,
-        title: name,
-        subTitle: description,
-      };
-    });
-  };
-
-  // const handleForm = (key, value) => {
-  // setForm((currentForm) => ({
-  //   ...currentForm,
-  //   [key]: value,
-  // }));
-  // };
-
-  const renderItem = (props) => {
-    return <ListRowItem key={props.index} {...props.item} />;
   };
 
   const renderListHeader = () => {
@@ -73,43 +27,57 @@ export const AddAddress = () => {
       <>
         <Box paddingVertical="s" paddingHorizontal="m">
           <TextField
-            borderColor={status === 1 ? 'red' : 'white'}
+            borderColor={title === '' && status === true ? 'red' : 'white'}
             inputProps={{
               placeholder: 'Titulo',
-              onChangeText: (text) => setTitle(text),
+              onChangeText: (text) => {
+                setTitle(text);
+                setStatus(true);
+              },
               defaultValue: title,
             }}
           />
-          {status === 1 ? (
+          {title === '' && status === true ? (
             <Text style={{ color: 'red', fontSize: 16 }}>
               {'Titulo não pode ficar em branco.'}
             </Text>
           ) : null}
-          {/* <Text style={styles.label}>Telefone</Text> */}
-          {/* <MaskedTextInput
-            mask="(99)99999-9999"
-            placeholder="(66)98101-0123"
-            onChangeText={(value) => handleForm('birthDay', value)}
-            keyboardType="numeric"
-            style={styles.input}
-          /> */}
           <Divider backgroundColor="card" marginVertical="s" />
           <TextField
+            borderColor={phone === '' && status === true ? 'red' : 'white'}
             inputProps={{
               placeholder: 'Telefone',
               keyboardType: 'numeric',
-              // onChangeText: (text) => formatPhoneNumber(text),
+              onChangeText: (text) => {
+                setPhone(text);
+                setStatus(true);
+              },
             }}
           />
+          {phone === '' && status === true ? (
+            <Text style={{ color: 'red', fontSize: 16 }}>
+              {'Telefone não pode ficar em branco.'}
+            </Text>
+          ) : null}
           <Divider marginVertical="s" />
           <TextField
+            borderColor={address === '' && status === true ? 'red' : 'white'}
             inputProps={{
               placeholder: 'Endereço',
               multiline: true,
               numberOfLines: 5,
+              onChangeText: (text) => {
+                setAddress(text);
+                setStatus(true);
+              },
             }}
             leftIcon="location"
           />
+          {address === '' && status === true ? (
+            <Text style={{ color: 'red', fontSize: 16 }}>
+              {'Endereço não pode ficar em branco.'}
+            </Text>
+          ) : null}
           <Divider backgroundColor="card" marginVertical="s" />
           <Button label="Salvar" isFullWidth onPress={saveClick} />
         </Box>
@@ -118,26 +86,26 @@ export const AddAddress = () => {
     );
   };
 
-  const styles = StyleSheet.create({
-    container: {},
-    paragraph: {},
-    card: {},
-    label: {
-      fontSize: 12,
-      marginTop: 5,
-    },
-    input: {
-      height: 40,
-      marginHorizontal: 0,
-      marginVertical: 5,
-      paddingHorizontal: 5,
-      borderWidth: 1,
-      borderRadius: 10,
-    },
-    buttonContainer: {
-      marginTop: 10,
-    },
-  });
+  // const styles = StyleSheet.create({
+  //   container: {},
+  //   paragraph: {},
+  //   card: {},
+  //   label: {
+  //     fontSize: 12,
+  //     marginTop: 5,
+  //   },
+  //   input: {
+  //     height: 40,
+  //     marginHorizontal: 0,
+  //     marginVertical: 5,
+  //     paddingHorizontal: 5,
+  //     borderWidth: 1,
+  //     borderRadius: 10,
+  //   },
+  //   buttonContainer: {
+  //     marginTop: 10,
+  //   },
+  // });
 
   // return (
   //   <List
