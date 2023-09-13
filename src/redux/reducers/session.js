134d864b1e07faaf02379ecf9ba633472f107a92
caeg0n <caegon2005@hotmail.com>
2023-09-13 00:52:48 -0300

    update

--- src/redux/reducers/session.js
+++ src/redux/reducers/session.js
@@ -1,11 +1,14 @@
 import { SET_USER_UUID } from '@src/redux/actions/session';
 import { SET_USER_EXPO_TOKEN } from '@src/redux/actions/session';
-import { GET_IS_REGISTERED } from '@src/redux/actions/session';
+import { SET_ADDRESSES } from '@src/redux/actions/session';
+import { SET_SELECTED_ADDRESSES } from '@src/redux/actions/session';
 import { RESET_UUID } from '@src/redux/actions/session';
 
 const initialState = {
   uuid: '',
   expo_token: '',
+  addresses: [],
+  selected_address: 0,
 };
 
 function sessionReducer(state = initialState, action) {
@@ -14,6 +17,10 @@ function sessionReducer(state = initialState, action) {
       return { ...state, uuid: action.payload };
     case SET_USER_EXPO_TOKEN:
       return { ...state, expo_token: action.payload };
+    case SET_ADDRESSES:
+      return { ...state, addresses: action.payload };
+    case SET_SELECTED_ADDRESSES:
+      return { ...state, selected_address: action.payload };
     case RESET_UUID:
       return { ...state, uuid: action.payload };
     default:
